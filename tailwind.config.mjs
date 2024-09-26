d05496df22ca271ae677822fbd40cb863ede11a9
Signe <sila0001@stud.kea.dk>
2024-09-26 15:01:47 +0200

    alt lavet fra start

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        beige: "#F1EFE9",
+        rød: "#C92A2A",
+        mørkerød: "#440606",
+      },
+      fontFamily: {
+        // Tilføj Kulim Park fonten her
+        kulim: ["Kulim Park", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
